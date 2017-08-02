addca0143e2d7537d03b8003fb3e9eaa330a8a2f
zhaozg117 <zhaozhigang117@163.com>
2017-08-02 23:11:10 +0800

    Update directive.js

--- app/static/js/directive/directive.js
+++ app/static/js/directive/directive.js
@@ -1,3 +1,30 @@
-/**
- * Created by Administrator on 2016/12/27.
- */
+define(['app'],function(app){
+    app.directive('dropbox', function(){
+
+        return {
+            restrict: 'E',
+            //scope: {},
+            link: function(scope, element, attrs) {
+                scope.displayMenu = false;
+
+                scope.showMenu = function(e) {
+                    scope.displayMenu = true;
+                    e.stopPropagation();
+                };
+
+                document.addEventListener('click', function(){
+                    scope.displayMenu = false;
+                    scope.$apply();
+                }, false);
+
+            },
+            template: '<div>  <input type="text" ng-click="showMenu($event)">'
+                +'<div ng-show="displayMenu"> ' +
+                 '<ul>'
+                +'<li>tesss</li>'
+                +'<li>ssss</li>'
+                +'<li>ggggggg</li>'
+               +'</ul> </div> </div>'
+        };
+    });
+})
